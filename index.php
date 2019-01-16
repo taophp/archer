0b0fea00fcefbfe72988005a23bb2ac12f98d306
Stéphane Mourey <stephane.mourey@impossible-exil.info>
2019-01-16 22:49:35 +0100

    Now, url can be registered

--- index.php
+++ index.php
@@ -1,27 +1,44 @@
 <?php
 
-$dsn = 'sqlite:'.dirname($_SERVER['SCRIPT_FILENAME']).'/db.sq3';
+$dsn = 'sqlite:'.dirname($_SERVER['SCRIPT_FILENAME']).'/db.sqlite';
 
 $db = new PDO($dsn);
-if ($_SERVER['REQUEST_URI'] === '/' && count($_GET)===0) {
+if ($_SERVER['REQUEST_URI'] === '/') {
+	if (isset($_POST['url']) && filter_var($_POST['url'])) {
+		$sql = 'INSERT INTO shorts values ('
+			. (
+				isset($_POST['short'])
+				? $db->quote($_POST['code'])
+				: '\'\''
+			)
+			. ','
+			. $db->quote($_POST['url'])
+			. ')'
+		;
+		if (!$db->exec($sql)) {
+			var_dump($sql,$db->errorCode());
+		}
+	}
 	printHeader();
 	echo <<<EOH
 	<h1>Archer Index</h1>
 	<h2>Ajout d'un raccourci</h2>
-	<form>
+	<form method="post">
 	<input name="url" placeholder="URL">
-	<!--input name="shortid" placeholder="Identifiant souhaité"-->
+	<input name="code" placeholder="Identifiant souhaité">
 	<input type="submit">
 	</form>
 	<h2>Raccourcis existants</h2>
 	<table>
-		<tr><th>id</th><th>Destination</th></tr>
+		<tr><th>id</th><th>Shortcut</th><th>Destination</th></tr>
 EOH;
-	$sql = 'SELECT * FROM shorts';
+	$sql = 'SELECT rowid,* FROM shorts';
 	$query = $db->query($sql);
 	$results = $query->fetchAll();
 	foreach ($results as $result) {
-		echo '<td>'.$result['id'].'</td><td>'.$result['url'].'</td>';
+		echo '<tr></tr><td>'.$result['rowid'].'</td><td>'
+			.($result['code'] ? $result['code'] : base64_encode($result['rowid']))
+			.'</td><td>'.$result['url'].'</td></tr>';
 	}
 	echo '</ul>';
 	printFooter();
